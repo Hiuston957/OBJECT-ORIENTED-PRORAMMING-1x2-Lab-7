069ce13f978025d5605dff2db501932e6336a6f2
Luke <lukasz.kutynski@microsoft.wsei.edu.pl>
2023-07-02 12:43:52 +0200

    Add project files.

---
+++ Exercise 4 – Writing files. StreamWriter class/Program.cs
@@ -0,0 +1,26 @@
+﻿//Use the StreamWriter class when you want to write line after line to a file.
+
+using System;
+using System.IO; // needed for StreamWriter
+namespace Ex_07_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // or string file = Directory.GetCurrentDirectory() + @"\powers.txt";
+            string file = "powers.txt";
+            using StreamWriter writer = new StreamWriter(file);
+            for (int i = 0; i <= 30; i++)
+            {
+                double power = Math.Pow(2, i); // 2^i
+                writer.WriteLine($"2^{i} = {power}");
+            }
+            // Some other variants of parameters for new StreamWriter(...)
+            // (file, append) -> append (true) or rewrite (false) file
+            // (file, append, enc) -> encoding, e.g. System.Text.Encoding.UTF8
+            // If you need non-locking access to a file, use this approach:
+            // await writer.WriteLineAsync("..."); // needs to be in an async method
+        }
+    }
+}
---
+++ OBJECT-ORIENTED-PRORAMMING-1x2-Lab-4/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Use the StreamReader class when you want to read line after line from a file.
+StreamWriter has also a ReadToEnd method that reads all lines, however in such a case,
+it may be simpler to use the File class.
+Links:
+• https://stackoverflow.com/a/3448307
+*/
+using System;
+using System.IO; // needed for StreamReader
+namespace Ex_07_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nextLine;
+            string file = "song.txt"; // if it is in the same folder as program.exe
+                                      // or use absolue path, e.g. @"C:\Users\..."
+                                      // Use the StreamWriter class when you want to read line after line
+                                      // "using" to correctly dispose the object afterwards
+            using StreamReader reader1 = new StreamReader(file);
+            for (int i = 1; ; i++)
+            {
+                nextLine = reader1.ReadLine();
+                if (nextLine is not null)
+                    Console.WriteLine(i + " " + nextLine);
+                else
+                    break;
+            }
+            Console.WriteLine();
+            // The same, shorter - if you don't need index i
+            using StreamReader reader2 = new StreamReader(file);
+            while ((nextLine = reader2.ReadLine()) is not null)
+                Console.WriteLine(nextLine + ", yo");
+            // If you need non-locking access to a file, use this approach:
+            // using FileStream stream = new FileStream(
+            // file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            // using StreamReader reader = new StreamReader(stream);
+            // reader.ReadLine();
+        }
+    }
+}
